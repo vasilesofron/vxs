b7c0a3087d8ebbe72bba8c88f7fe0e834a02aea7
Vasile Sofron <vasilesofron01@gmail.com>
2023-01-20 19:09:12 +0200

    Added Profile Page Form

--- lib/nav_screens/profile/profile.dart
+++ lib/nav_screens/profile/profile.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:vxs/login/login_page.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
@@ -26,12 +27,32 @@ class Profile extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           child: Column(children: [
-            SizedBox(
-              width: 120,
-              height: 150,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.circular(100),
-                  child: Image(image: AssetImage("img/user.png"))),
+            Stack(
+              children: [
+                SizedBox(
+                  width: 120,
+                  height: 150,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image(image: AssetImage("img/user.png"))),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: Color(0xff34b982).withOpacity(0.5),
+                          ),
+                          child: Icon(
+                            Icons.camera_alt_rounded,
+                            color: Color(0xfffff2d1),
+                            size: 20,),
+                        ),
+                ),
+              ],
             ),
             SizedBox(
               height: 10,
@@ -53,21 +74,101 @@ class Profile extends StatelessWidget {
                         color: Color(0xff34b982),),
                         
                         ),
-                     child: const Text("Edit Profile"),)
+                     child: const Text("Save Profile"),)
                     ),
                     SizedBox(height: 30,),
                     Divider(),
 
                     ///MENU
-                    ListTile(
-                      leading: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          color: Color(0xff34b982).withOpacity(0.5),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Form(child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text('Full Name'),
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person_outline_rounded,
+                            color: Color(0xfffff2d1),
+                            ),
+                            labelStyle: TextStyle(
+                              color: Color(0xfffff2d1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0, color: Color(0xfffff2d1)),
+                            )
+                          ),
                         ),
-                        child: Icon(Icons.abc),
+                        SizedBox(height: 10,),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text('Farm Name'),
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.house_rounded,
+                            color: Color(0xfffff2d1),
+                            ),
+                            labelStyle: TextStyle(
+                              color: Color(0xfffff2d1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0, color: Color(0xfffff2d1)),
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text('Email'),
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.email_rounded,
+                            color: Color(0xfffff2d1),
+                            ),
+                            labelStyle: TextStyle(
+                              color: Color(0xfffff2d1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0, color: Color(0xfffff2d1)),
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            label: Text('Password'),
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.key_rounded,
+                            color: Color(0xfffff2d1),
+                            ),
+                            labelStyle: TextStyle(
+                              color: Color(0xfffff2d1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0, color: Color(0xfffff2d1)),
+                            )
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            label: Text('Confirm Password'),
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.key_rounded,
+                            color: Color(0xfffff2d1),
+                            ),
+                            labelStyle: TextStyle(
+                              color: Color(0xfffff2d1),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2.0, color: Color(0xfffff2d1)),
+                            )
+                          ),
+                        ),
+                        ],
+                      )
                       ),
                     )
                     
